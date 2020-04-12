6903fe85aa468d28d94b60f6369ac881f2eee1dc
Axel C <ac@insightapps.io>
2020-04-12 23:14:33 +0700

    Updates

---
+++ precache-manifest.6f88e386996676bb3b2ebacda6844f6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c23f2b42cb41a6513f9d5caf785b845",
+    "url": "/monster-list/index.html"
+  },
+  {
+    "revision": "bf8c24082a0dc46ecf8d",
+    "url": "/monster-list/static/css/main.3f4dacb4.chunk.css"
+  },
+  {
+    "revision": "d917d29af99285196825",
+    "url": "/monster-list/static/js/2.9cd64d92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-list/static/js/2.9cd64d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8c24082a0dc46ecf8d",
+    "url": "/monster-list/static/js/main.953e7001.chunk.js"
+  },
+  {
+    "revision": "157b318469794d28e11e",
+    "url": "/monster-list/static/js/runtime-main.1c4125a0.js"
+  }
+]);
